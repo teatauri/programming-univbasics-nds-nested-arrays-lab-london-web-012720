173ccae3c93fa15eb78c13f0509490b9b16c6dc4
Lee Bardon <leerbardon@gmail.com>
2019-12-21 23:35:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,7 +18,10 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
     
-    nested = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
+    nested = [
+          CONVENTIONAL_PRODUCE, 
+          ORGANIC_PRODUCE 
+          ]
     
     
   # Build an array that contains both of the above arrays
